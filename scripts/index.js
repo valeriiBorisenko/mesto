46cc6405152aadcb170b53745b9d5385b957eb6d
Krotpeshehod <tjvell@mail.ru>
2020-11-25 00:29:59 +0300

    feat: сделать javascript для редактирования профиля

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,14 @@
 const profileButtonEdit = document.querySelector('.profile__edit-button');
 const popupButtonClose = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup')
+const popupVisible = document.querySelector('.popup_visible');
 
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+const popupWindow = document.querySelector('.popup__window')
+let popupFormTitle = document.querySelector('.popup__form-title');
+let popupFormSubtitle = document.querySelector('.popup__form-subtitle');
+const popupSaveButton = document.querySelector('.popup__save-button')
 
 profileButtonEdit.addEventListener('click', togglePopupVisibility);
 popupButtonClose.addEventListener('click', togglePopupVisibility);
@@ -9,3 +16,21 @@ popupButtonClose.addEventListener('click', togglePopupVisibility);
 function togglePopupVisibility(){
   popup.classList.toggle('popup_visible');
 }
+
+popupSaveButton.addEventListener('click', removePopupWindowSubmit);
+
+  function removePopupWindowSubmit(){
+    popup.classList.remove('popup_visible');
+}
+
+popupFormTitle.value = profileTitle.textContent;
+popupFormSubtitle.value = profileSubtitle.textContent;
+
+popupWindow.addEventListener('submit', popupWindowSubmit);
+
+function popupWindowSubmit(event){
+  event.preventDefault();
+  profileTitle.textContent = popupFormTitle.value;
+  profileSubtitle.textContent = popupFormSubtitle.value;
+}
+
