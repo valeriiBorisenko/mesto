ab1a8c9df511a538c9d20e4fa4f4e08c93eaf833
Krotpeshehod <tjvell@mail.ru>
2020-12-17 03:10:25 +0300

    fix: js and html after 1 review

---
+++ scripts/initial-element.js
@@ -0,0 +1,27 @@
+const initialElement = [
+    {
+        name: 'Поездка на море с досками',
+        link: './image/elements/van.jpeg'
+    },
+    {
+        name: 'Доброе утро',
+        link: './image/elements/morning.jpeg'
+    },
+    {
+        name: 'Свежий кофе у костра',
+        link: './image/elements/coffee.jpeg'
+    },
+    {
+        name: 'Вид из машины',
+        link: './image/elements/view_from_van.jpeg'
+    },
+    {
+        name: 'Горное озеро',
+        link: './image/elements/boat.jpeg'
+    },
+    {
+        name: 'Подготовка к путешествию',
+        link: './image/elements/road_trip.jpeg'
+    }
+  ];
+  
